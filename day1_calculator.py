59a39411006e9c206a7ddc41398c0ff0d64a4909
ShreyaSarma21 <sarmashreya02@gmail.com>
2025-04-12 15:14:09 +0530

    Day 1: Calculator program

---
+++ day1_calculator.py
@@ -0,0 +1,6 @@
+name = input("Enter name:")
+print(f"Hello,{name}!")
+
+num1 = float(input("Enter 1st num:"))
+num2 = float(input("Enter 2nd num:"))
+print(f"The sum is : {num1}+{num2} = {num1+num2}")
---
+++ week1.py
@@ -0,0 +1,43 @@
+#Variables hold data
+name = "Shreya"     #str
+age = 22            #int
+height = 5.1        #float
+is_student = True   #bool
+
+#Printing & input
+#Output
+print("Charlie Puth, uth..")
+#Input
+user_name = input("What's your name?")
+print("Hi",user_name,"ab uth jaa")
+
+#Basic Math
+a = 5
+b = 2
+print("Sum:",a+b)
+print("Division:",a/b)
+print("Floor Division:", a//b)
+print("Power",a ** b)
+
+
+#🧪 MINI TASK: Hello & Calculator
+# Create a program that:
+# Greets the user by name
+# Asks for 2 numbers
+# Prints the sum
+
+name = input("Enter name:")
+print(f"Hello,{name}!")
+
+num1 = float(input("Enter 1st num:"))
+num2 = float(input("Enter 2nd num:"))
+print(f"The sum is : {num1}+{num2} = {num1+num2}")
+
+
+
+
+
+
+
+
+
