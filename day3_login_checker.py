b87b77adc5c909a9bc94b0c6b7db7702b3424fca
ShreyaSarma21 <sarmashreya02@gmail.com>
2025-04-13 05:08:40 +0530

    Enhance login checker: add 3 attempts and masked password

--- day3_login_checker.py
+++ day3_login_checker.py
@@ -1,10 +1,23 @@
-username = input("Enter your username: ").lower()
-password = input("Enter your password: ")
+import getpass  # used to hide password input in terminal
 
+# Set the correct credentials
+correct_username = "shreya"
+correct_password = "python123"
 
-if username == "shreya" and password == "python123":
-    print("Login successful, Welcome Shreya!")
+# Allow user up to 3 login attempts
+for attempt in range(3):
+    print(f"\nAttempt {attempt + 1} of 3")
+
+    username = input("Enter username: ").strip().lower()
+    password = getpass.getpass("Enter password: ").strip()
+
+    if username == correct_username and password == correct_password:
+        print("Login successful. Welcome, Shreya!")
+        break
+    else:
+        print("Invalid credentials.")
+
+# If the user fails 3 times
 else:
-    print("Invalid credentials.")
-
+    print("\n Too many failed attempts. Access denied.")
 
